bb0004ca1b0644ebe18acc518de97e8edfa5b823
Landon Safron <lsafron@ualberta.ca>
2017-04-21 11:12:11 -0600

    Worked on Sensitivity.jl

--- src/Operators/Sensitivity.jl
+++ src/Operators/Sensitivity.jl
@@ -1,25 +1,71 @@
-function Sensitivity()
+
+    ####### NEED TO TAKE THE REAL PART OF OUT???????????????
+
+
+    #### HOW TO INPUT SPARSE MATRICES INTO A FUNCTION WITH PREDEFINED TYPES???????
+
+
+function Sensitivity{T<:Complex}()
 
 # This function is the forward and adjoint sensitivity/jacobian operator
 # for multiple shots.
+#
+# INPUTS:     IN        - Data residuals for adjoint; Model perturbation for forward
+#             adj       - true for adjoint; false for forward
+#             R         - Restriction operator
+#             H         - LU factorization of the Helmholtz operator
+#             U         - Predicted forward-propagated wavefield for each shot (stored as a matrix with dimensions nz*nx,ns)
+#             A         - Diagonal matrix containing the complex-valued attenuation coefficients
+#
+# OUTPUTS:    OUT       - Model perturbation for adjoint; Data residuals for forward
 
-    for ishot = 1:ns # DO THE OPERATION 1 SHOT AT A TIME
-        SOMETHING = Sensitivity()
+    ng = size(R,1)
+    ns = size(U,2)
+    n = size(A,2)
+
+    if adj==false
+        OUT = zeros(T,ng*ns)
+        for ishot = 1:ns
+            OUT[(ishot-1)*ng+1:ishot*ng] = Sensitivity(IN,adj,R=R,H=H,U=U[:,ishot],A=A)
+        end
     end
+
+    if adj==true
+        OUT = zeros(T,n)
+        for ishot = 1:ns
+            OUT += Sensitivity(IN[(ishot-1)*ng+1:ishot*ng],adj,R=R,H=H,U=U[:,ishot],A=A)
+        end
+    end
+
+    return OUT
 
 end
 
 
 
 
-function Sensitivity()
+function Sensitivity{T<:Complex}(IN::Array{T,1},adj::Bool;R::Array{Int,2},H,U::Array{T,1},A::Array{T,1})
 
 # This function is the forward and adjoint sensitivity/jacobian operator
 # for a single shot.
+#
+# INPUTS:     IN        - Data residual for adjoint; Model perturbation for forward
+#             adj       - true for adjoint; false for forward
+#             R         - Restriction operator
+#             H         - LU factorization of the Helmholtz operator
+#             U         - Predicted forward-propagated wavefield
+#             A         - Diagonal matrix containing the complex-valued attenuation coefficients
+#
+# OUTPUTS:    OUT       - Model perturbation for adjoint; Data residual for forward
 
-# S = real(R*dudm) = real( -w^2 * R * inv(H) * diag(U) * diag(A) )
-# S' = real( -w^2 * diag(A)' * diag(U)' * inv(H)' * R' )
+    if adj==false
+        OUT = -w^2*R*H\(spdiagm(U)*A*IN)
+    end
 
-    # grad = -real(w^2*A'*spdiagm(U)'*(H'\(R'*r)))
+    if adj==true
+        OUT = -w^2*A'*spdiagm(U)'*(H'\(R'*IN))
+    end
+
+    return OUT
 
 end
